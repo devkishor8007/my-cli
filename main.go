aa9c778f70fa91ac13974df224557b63ef1a49ac
Kishor KC <kishor.kc@ekbana.info>
2023-11-30 00:03:07 +0545

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var (
+	userName string
+	showUser bool
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "simple",
+	Short: "simple cli with cobra",
+	Long: `Creating a simple CLI with Cobra and Golang
+	to explore and create something useful.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if userName != "" && showUser {
+			fmt.Println("Username: " + userName)
+		} else if userName != "" {
+			fmt.Println("Hello", userName)
+		} else {
+			fmt.Println("Please provide a valid username using the -m flag.")
+		}
+	},
+  }
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&userName, "msg", "m", "", "please enter a valid userName")
+	rootCmd.PersistentFlags().BoolVarP(&showUser, "showUser", "i", false, "Display the userName if valid")
+}
+
+func main() {
+	err := rootCmd.Execute()
+
+	if err != nil {
+		os.Exit(1)
+	}
+}
